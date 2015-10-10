b9027e0811e7ece572852dd88a78f2085c273e0e
esalling23 <esalling23@gmail.com>
2015-10-10 03:51:08 -0400

    first commit

---
+++ ReEarth/Assets/Scripts/ClickOnFaceScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickOnFaceScript : MonoBehaviour {
+
+	// Public fields are visible and their values can be changed dirrectly in the editor
+		// represents the position displacement needed to compute the position of the new instance
+		public Vector3 delta;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	// This function is triggered when the mouse cursor is over the GameObject on which this script runs
+	void OnMouseOver()
+	{
+		// If the left mouse button is pressed
+		if (Input.GetMouseButtonDown(0))
+		{
+			// Display a message in the Console tab
+			Debug.Log("Left click!");
+
+
+
+			// Destroy the parent of the face we clicked
+			//Destroy(this.transform.parent.gameObject);
+		}
+		
+		// If the right mouse button is pressed
+		if (Input.GetMouseButtonDown(1))
+		{
+			// Display a message in the Console tab
+			Debug.Log("Right click!");
+
+			// Call method from WorldGenerator class
+			WorldGenerator.CloneAndPlace(this.transform.parent.transform.position + delta, // N = C + delta
+			                             this.transform.parent.gameObject); // The parent GameObject 
+		}
+	}
+
+}
---
+++ ReEarth/Assets/Scripts/OnClickCubeUI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClickCubeUI : MonoBehaviour {
+
+	public GameObject UIInfo;
+	public GameObject UITools;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnMouseOver()
+	{
+		// If the left mouse button is pressed
+		if (Input.GetMouseButtonDown(0))
+		{
+			// Display a message in the Console tab
+			Debug.Log("Left click!");
+
+			UIInfo.SetActive(true);
+			// Destroy the parent of the face we clicked
+			//Destroy(this.transform.parent.gameObject);
+		}
+		
+		// If the right mouse button is pressed
+		if (Input.GetMouseButtonDown(1))
+		{
+			// Display a message in the Console tab
+			Debug.Log("Right click!");
+
+			UITools.SetActive(true);
+
+			// Call method from WorldGenerator class
+			//WorldGenerator.CloneAndPlace(this.transform.parent.transform.position + delta, // N = C + delta
+			                             //this.transform.parent.gameObject); // The parent GameObject 
+		}
+	}
+
+}
